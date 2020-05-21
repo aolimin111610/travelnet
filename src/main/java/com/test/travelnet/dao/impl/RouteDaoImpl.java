f52896bcde424dfb2d95849b2ae152f183e8b559
liminao <liminao@qq.com>
2020-05-21 19:57:20 +0800

    分页数据修改

--- src/main/java/com/test/travelnet/dao/impl/RouteDaoImpl.java
+++ src/main/java/com/test/travelnet/dao/impl/RouteDaoImpl.java
@@ -18,13 +18,13 @@ public class RouteDaoImpl implements RouteDao {
     private JdbcTemplate template= new JdbcTemplate(JDBCUtils.getDataSource());
     @Override
     public int findTotalCount(int cid) {
-        String sql ="select count(*) form tab_route where cid = ?";
+        String sql = "select count(*) from tab_route where cid = ?";
         return template.queryForObject(sql,Integer.class,cid);
     }
 
     @Override
     public List<Route> findByPage(int cid, int start, int pageSize) {
-        String sql = "select  * from tab_route where cid = ? limit ?,?";
+        String sql = "select * from tab_route where cid = ? limit ? , ?";
         return template.query(sql,new BeanPropertyRowMapper<Route>(Route.class),cid,start,pageSize);
     }
 }
